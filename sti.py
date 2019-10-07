3624667d171149fe378699e28697ffe4c36e8869
Mr robot <30386159+edmark21@users.noreply.github.com>
2019-10-07 19:39:02 +0800

    Update sti.py

--- sti.py
+++ sti.py
@@ -18,11 +18,11 @@ logo = '''
 
 print logo
 lname = raw_input("Enter Lastname: ")
-sid = raw_raw_input("Enter 11 digit Student#: ")
-city = raw_raw_input("Enter City: ")
+sid = raw_input("Enter 11 digit Student#: ")
+city = raw_input("Enter City: ")
 yr = raw_input("Enter Birth Year: ")
 mt = raw_input("Enter Birth Month: ")
 dt = raw_input("Enter Birth Date: ")
-lu = lname.upper(0)
+
 print ("\nUsername: " + lname + "." + sid[5:-1] + sid[10] + "@" + city + ".sti.edu.ph")
 print ("Password: " + lname + yr + mt + dt)
